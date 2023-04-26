40f1bc18679cb1eae9902e558b1d26e19dd22e95
Palash Halder <halder.palash08@gmail.com>
2023-04-26 07:58:31 +0600

    update deposit.js page

---
+++ js/deposit.js
@@ -0,0 +1,8 @@
+// tp-1: add event listener to the deposit button
+document.getElementById('btn-deposit').addEventListener('click', function() {
+    // step-2 get the deposit amount from the deposit input field
+    // for input field use .value to get the value inside the input field
+    const depositField = document.getElementById('deposit-field');
+    const depositAmount = depositField.value;
+    console.log(depositAmount);
+})
